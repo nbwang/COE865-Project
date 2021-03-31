5fcecf4e2cc2b5d67c0f2bba388bca399fcd4a2c
nbwang <33729243+nbwang@users.noreply.github.com>
2021-03-31 18:18:00 -0400

    added graphs and bellmanford alg

--- src/multicastnetwork/BellmanFord.java
+++ src/multicastnetwork/BellmanFord.java
@@ -11,7 +11,28 @@ import java.io.*;
 
 
 public class BellmanFord {
-    public void BellmanFord(Graph graph, Node source){
-        
+    public static ArrayList<Node> BellmanFord(Graph graph, Node Source, Node Destination){
+        ArrayList<Node> path = new ArrayList<Node>();
+        path.add(Source);
+        int numberOfVertices = graph.NodeList.size();
+
+        //Creates an array to hold the destination distances
+        int dist[] = new int[numberOfVertices];
+        for(int i = 0; i < numberOfVertices; i++){
+            dist[i] = Integer.MAX_VALUE;
+        }
+        dist[Source.getId()] = 0;
+
+        for(int  i = 1; i < graph.NodeList.size(); i++){
+            for(Edge e : graph.EdgeList){
+                if(dist[e.getSource().getId()] != Integer.MAX_VALUE &&  (dist[e.getSource().getId()] + e.getWeight()) < dist[e.getDestination().getId()]){
+                    if(dist[e.getDestination().getId()] == dist[Destination.getId()]){
+                        path.add(e.destination);
+                    }
+                    dist[e.getDestination().getId()] = dist[e.getSource().getId()] + e.getWeight();
+                }
+            }
+        }
+        return path;
     }
 }
--- src/multicastnetwork/Graph.java
+++ src/multicastnetwork/Graph.java
@@ -11,4 +11,87 @@ import java.io.*;
 
 
 public class Graph {
+    public static void main (String args[])
+    {
+        ArrayList<Node> nodesList = new ArrayList<Node>();
+        ArrayList<Edge> edgesList = new ArrayList<Edge>();
+        boolean linebreak = false;
+        int line = 0;
+        File topology = new File ("topology.txt");
+        // Source source;
+
+        try{
+            Scanner fileReader = new Scanner(topology);
+            while (fileReader.hasNextLine())
+            {
+                line++;
+                String lineData = fileReader.nextLine();
+                if (lineData.equals("EDGES"));
+                {
+                    linebreak = true;
+                    continue;
+                }
+                if (linebreak == false)
+                {
+                    String stParts [] = lineData.split(" ");
+                    int nodeID = Integer.parseInt(stParts[0]);
+                    char nodeType = stParts[1].charAt(0);
+                    int nodePort = Integer.parseInt(stParts[2]);
+                    switch(nodeType){
+                        case 'S':
+                            Source source = new Source(nodeID, nodePort);
+                            nodesList.add(source);
+                        case 'R':
+                            Reciever reciever = new Receiver(nodeID, nodePort);
+                            nodesList.add(reciever);
+                        case 'F':
+                            Forwarder forwarder = new Forwarder(nodeID, nodePort);
+                            nodesList.add(forwarder);
+                    }
+                }
+                else
+                {
+                    // edgesList.add(lineData);
+                    String stParts [] = lineData.split(" ");
+                    int edgeWeight = Integer.parseInt(stParts[0]);
+                    int edgeSrc = Integer.parseInt(stParts[1]);
+                    int edgeDest = Integer.parseInt(stParts[2]);
+
+                    Node src;
+                    Node dest;
+                    for(Node n: nodesList){
+                        if(n.getId() == src){
+                            src = n;
+                        }
+                        else if (n.getId() == dest){
+                            dest = n;
+                        }
+                    }
+
+                    Edge edge = new Edge(edgeWeight, edgeSrc, edgeDest);
+                    edgesList.add(edge);
+                
+                }
+            }
+            fileReader.close();
+        }
+        catch (IOException e)
+        {
+            System.out.println (e.getMessage());
+        }
+    }
+
+    class Edge {
+        int weight;
+        Node src;
+        Node dest;
+
+        public Edge(int weight, Node src, Node dest){
+            this.weight = weight;
+            this.src = src;
+            this.dest = dest;
+        }
+                   
+    }
+
 }
