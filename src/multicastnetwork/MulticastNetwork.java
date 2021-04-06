07afdea0333378cfe655ccfa0e230132fe7ef6d0
nbwang <33729243+nbwang@users.noreply.github.com>
2021-04-05 21:11:58 -0400

    Editted multicastnetwork.java

--- src/multicastnetwork/MulticastNetwork.java
+++ src/multicastnetwork/MulticastNetwork.java
@@ -1,9 +1,6 @@
 package multicastnetwork;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class MulticastNetwork
 {
@@ -55,178 +52,102 @@ public class MulticastNetwork
         for (Receiver r : recvrList){
             source.generateShortestPath(bellmanFord.BellmanFordAlgorithm(graph, source, r));
         }
-
-
-        boolean check = false;
-        for (int i = 0 ; i < source.getShortestPaths().size() ; i++)
-        {
-            for (int j = 0 ; j < source.getShortestPaths().get(i).size() - 1 ; j++)
-            {
+        
+        int nodeIndex;
+        for (ArrayList<Node> list: source.getShortestPaths()){
+            for (Node n : list){
+                nodeIndex = list.indexOf(n);
                 //If node in path is equal to source
-                boolean sourceCheck = false;
-                check = false;
-                if (source.getShortestPaths().get(i).get(j).equals(source))
-                {
-                    check = false;
-                    for (int k = 0 ; k < forwarderList.size() ; k++)
-                    {
-                        if (source.getShortestPaths().get(i).get(j + 1).equals(forwarderList.get(k)))
-                        {
-                            if (source.getmCastGroup().getRecvrList().contains(forwarderList.get(k)) == false)
-                            {
-                                source.getmCastGroup().getRecvrList().add(forwarderList.get(k));
-                                forwarderList.get(k).getApartOfAddresses().add(source.getmCastGroup().getmAddr());
-                            }
-                            check = true;
-                            sourceCheck = true;
+                if (n.equals(source)){
+                    for (Forwarder f :forwarderList){
+                        if (list.get(nodeIndex + 1).equals(f) && !source.getmCastGroup().getRecvrList().contains(f)){
+                            source.getmCastGroup().getRecvrList().add(f);
+                            f.getApartOfAddresses().add(source.getmCastGroup().getmAddr());
                             break;
                         }
                     }
-                    if (check == false)
-                    {
-                        for (int k = 0 ; k < recvrList.size() ; k++)
-                        {
-                            if (source.getShortestPaths().get(i).get(j + 1).equals(recvrList.get(k)))
-                            {
-                                if (source.getmCastGroup().getRecvrList().contains(recvrList.get(k)) == false)
-                                {
-                                    source.getmCastGroup().getRecvrList().add(recvrList.get(k));
-                                    recvrList.get(k).getApartOfAddresses().add(source.getmCastGroup().getmAddr());
-                                }
-                                sourceCheck = true;
-                                break;
-                            }
-                        }
-                    }
                 }
-
-                if (sourceCheck == true)
-                {
-                    continue;
-                }
-
-                check = false;
                 //If node in path is equal to a node in forwarder list
-                for (int n = 0 ; n < forwarderList.size() ; n++)
-                {
-                    if (source.getShortestPaths().get(i).get(j).equals(forwarderList.get(n)))
-                    {
-                        check = false;
-                        for (int m = 0 ; m < forwarderList.size() ; m++)
-                        {
-                            if (source.getShortestPaths().get(i).get(j + 1).equals(forwarderList.get(m)))
-                            {
-                                if (forwarderList.get(n).getmCastGroup().getRecvrList().contains(forwarderList.get(m)) == false)
-                                {
-                                    forwarderList.get(n).getmCastGroup().getRecvrList().add(forwarderList.get(m));
-                                    forwarderList.get(m).getApartOfAddresses().add(forwarderList.get(n).getmCastGroup().getmAddr());
-                                }
-                                check = true;
+                for (Forwarder f :forwarderList){
+                    if (n.equals(f)){
+                        for (Forwarder f2: forwarderList){
+                            if (list.get(nodeIndex + 1).equals(f2) && !f.getmCastGroup().getRecvrList().contains(f2)){
+                                f.getmCastGroup().getRecvrList().add(f2);
+                                f2.getApartOfAddresses().add(f.getmCastGroup().getmAddr());
                                 break;
                             }
                         }
-
-                        if (check == false)
-                        {
-                            for (int m = 0 ; m < recvrList.size() ; m++)
-                            {
-                                if (source.getShortestPaths().get(i).get(j + 1).equals(recvrList.get(m)))
-                                {
-                                    if (forwarderList.get(n).getmCastGroup().getRecvrList().contains(recvrList.get(m)) == false)
-                                    {
-                                        forwarderList.get(n).getmCastGroup().getRecvrList().add(recvrList.get(m));
-                                        recvrList.get(m).getApartOfAddresses().add(forwarderList.get(n).getmCastGroup().getmAddr());
-                                    }
-                                    check = true;
-                                    break;
-                                }
+                        for (Receiver r : recvrList){
+                            if (list.get(nodeIndex + 1).equals(r) && !f.getmCastGroup().getRecvrList().contains(r)){
+                                f.getmCastGroup().getRecvrList().add(r);
+                                r.getApartOfAddresses().add(f.getmCastGroup().getmAddr());
+                                break;
                             }
                         }
-                    }
-                    if (check == true)
-                    {
-                        break;
                     }
                 }
             }
         }
 
-        // System.out.println (source);
-        // System.out.println ("");
+        System.out.println (source);
+        System.out.println ("");
 
-        // for (int i = 0 ; i < forwarderList.size() ; i++)
-        // {
-        //     System.out.println (forwarderList.get(i));
-        //     System.out.println ("");
-        // }
-        // for (int i = 0 ; i < recvrList.size() ; i++)
-        // {
-        //     System.out.println (recvrList.get(i));
-        //     System.out.println ("");
-        // }
+        for (Forwarder f :forwarderList){
+            System.out.println (f);
+            System.out.println ("");
+        }
+        for (Receiver r : recvrList){
+            System.out.println (r);
+            System.out.println ("");
+        }
 
         System.out.println (source.getAllShortestPaths());
 
         Thread sourceThread = new Thread(source);
         source.setupAllSockets();
-        ArrayList<Thread> forwardThreadList = new ArrayList<Thread>();
+        ArrayList<Thread> forwarderThreadList = new ArrayList<Thread>();
         ArrayList<Thread> recvrThreadList = new ArrayList<Thread>();
 
-        for (int i = 0 ; i < forwarderList.size() ; i++)
-        {
-            forwarderList.get(i).setupAllSockets();
-            forwardThreadList.add(new Thread(forwarderList.get(i)));
+        for (Forwarder f :forwarderList){
+            f.setupAllSockets();
+            Thread fThread = new Thread(f);
+            forwarderThreadList.add(fThread);
+            fThread.start();
         }
 
-        for (int i = 0 ; i < recvrList.size() ; i++)
-        {
-            recvrList.get(i).setupAllSockets();
-            recvrThreadList.add(new Thread(recvrList.get(i)));
+        for (Receiver r : recvrList){
+            r.setupAllSockets();
+            Thread rThread = new Thread(r);
+            recvrThreadList.add(rThread);
+            rThread.start();
         }
-
-        for (int i = 0 ; i < recvrThreadList.size() ; i++)
-        {
-            recvrThreadList.get(i).start();
-        }
-
-        for (int i = 0 ; i < forwardThreadList.size() ; i++)
-        {
-            forwardThreadList.get(i).start();
-        }
-
         sourceThread.start();
-        try
-        {
+
+        try{
             sourceThread.join();
 
-            for (int i = 0 ; i < recvrThreadList.size() ; i++)
-            {
-                recvrThreadList.get(i).join();
+            for (Thread thread: forwarderThreadList){
+                thread.join();
             }
 
-            for (int i = 0 ; i < forwardThreadList.size() ; i++)
-            {
-                forwardThreadList.get(i).join();
+            for (Thread thread: recvrThreadList){
+                thread.join();
             }
         }
-        catch (InterruptedException e)
-        {
+        catch (InterruptedException e){
             System.out.println (e.getMessage());
         }
 
         System.out.println ("PACKET TRACING:\n");
 
-        for (int i = 0 ; i < forwarderList.size() ; i++)
-        {
-            if (forwarderList.get(i).getApartOfAddresses().isEmpty() != true)
-            {
+        for (int i = 0 ; i < forwarderList.size() ; i++){
+            if (!forwarderList.get(i).getApartOfAddresses().isEmpty()){
                 forwarderList.get(i).getOut();
             }
 
         }
 
-        for (int i = 0 ; i < recvrList.size() ; i++)
-        {
+        for (int i = 0 ; i < recvrList.size() ; i++){
             recvrList.get(i).getOut();
         }
     }
